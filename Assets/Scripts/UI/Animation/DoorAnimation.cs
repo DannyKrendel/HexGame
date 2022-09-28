1bdc1ee9a0d8917bbbd645da1cb02d1ecd6cc350
Danny <danyl1607@gmail.com>
2022-09-29 01:51:49 +0300

    add door animation

---
+++ Assets/Scripts/UI/Animation/DoorAnimation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using DG.Tweening;
+using HexGame.Gameplay;
+using UnityEngine;
+
+namespace HexGame.UI.Animation
+{
+    public class DoorAnimation : MonoBehaviour
+    {
+        [SerializeField] private Door _door;
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private float _duration = 0.5f;
+        [SerializeField] private Ease _ease = Ease.InOutSine;
+
+        private float _openAlpha = 0.5f;
+        private Tween _openTween;
+        private Tween _closeTween;
+        
+        private void Awake()
+        {
+            _openTween = _spriteRenderer
+                .DOFade(_openAlpha, _duration)
+                .SetEase(_ease)
+                .Pause();
+            _closeTween = _spriteRenderer
+                .DOFade(1, _duration)
+                .SetEase(_ease)
+                .Pause();
+        }
+
+        private void OnEnable()
+        {
+            _door.Opened += PlayOpen;
+            _door.Closed += PlayClose;
+            _openTween.Rewind();
+            _closeTween.Rewind();
+        }
+        
+        private void OnDisable()
+        {
+            _door.Opened -= PlayOpen;
+            _door.Closed -= PlayClose;
+        }
+
+        private void PlayOpen()
+        {
+            _openTween.Restart();
+        }
+
+        private void PlayClose()
+        {
+            _closeTween.Restart();
+        }
+    }
+}
